d6152988219eb5880dee6f039ab2b4f75dbb4afc
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2024-09-15 12:38:52 -0300

    Grupo de estilização

--- App.js
+++ App.js
@@ -1,40 +1,31 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 
 class App extends Component{
-  
-  constructor(props){
-    super(props);
-    this.state = {
-      nome: ''
-    };
-    // Deixar a função acessivel para todas as propriedades
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar(nome){
-    this.setState({
-      nome: nome
-    })
-  }
-  
   render(){
     return(
-      <View style = {{marginTop: 150}}>
-        {/* Pode se criar uma função anonima ou pode acessar
-            uma função externa, primeiro se cria no construtor
-            para pode ter acesso;
-         */}
-        <Button title="Entrar" onPress={() => this.entrar('Algum texto')}/>
-
-        <Text style = {{fontSize: 45, color: 'red', textAlign: 'center', justifyContent: 'center'}}>
-            {this.state.nome}
-        </Text>
-
+      <View style= {styles.area}>
+        <Text style={[ styles.textoPrincipal, styles.alinharTexto ]}>Eu sou texto 1</Text>
+        <Text style = {styles.alinharTexto}>Eu sou texto 2</Text>
+        <Text >Eu sou texto 3</Text>
+        <Text style = {styles.textoPrincipal}>Eu sou texto 4</Text>
       </View>
     );
 }
 }
+
+const styles = StyleSheet.create({
+  area: {
+    marginTop: 40
+  },
+  textoPrincipal:{
+    fontSize: 25,
+    color: '#ff0000'
+  },
+  alinharTexto:{
+    textAlign: 'center'
+  }
+});
 
 export default App;
