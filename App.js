4ce70de87caa3a2f2ce2b77b680495f664db1d71
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2024-09-15 15:07:55 -0300

    Adicionando entrada do usuario

--- App.js
+++ App.js
@@ -1,28 +1,55 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: ' '
+    };
+    this.pegaNome = this.pegaNome.bind(this);
+  }
+
+  pegaNome(texto){
+    this.setState({nome: texto})
+  }
+
   render(){
     return(
-      <View style = {{flex: 1, flexDirection: 'collum', 
-                
-                //  justificando a coluna
-                      alignItems: 'stretch',
-                      
-                      backgroundColor: 'white'}}>
-        
-        <View style= {{height: 50, backgroundColor: 'green'}}></View>
-        <View style= {{height: 50, backgroundColor: 'red'}}></View>
-        <View style= {{height: 50, backgroundColor: 'yellow'}}></View>
-        <View style= {{height: 50, backgroundColor: 'green'}}></View>
-
-
+      <View>
+        <TextInput style = {styles.input}
+        placeholder="Digite seu nome"
+        underlineColorAndroid='transparent'
+        onChangeText={this.pegaNome}
+        />
+        <Text style= {styles.texto}>
+            Bem vindo: {this.state.nome}
+          </Text>
 
       </View>
     );
 }
 }
 
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+  texto:{
+    display: 'flex',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  input:{
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 10,
+    marginTop: 56,
+    fontSize: 20,
+    padding: 10,
+  },
+});
 
 export default App;
