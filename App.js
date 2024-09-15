3553a4fe9a011d3cea54637785a8343fba1ed3d8
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2024-09-15 14:09:05 -0300

    Finalizando flexbox

--- App.js
+++ App.js
@@ -5,12 +5,19 @@ import { View, Text, StyleSheet } from "react-native";
 class App extends Component{
   render(){
     return(
-      <View style = {{flex: 1, backgroundColor: '#222'}}>
+      <View style = {{flex: 1, flexDirection: 'collum', 
+                
+                //  justificando a coluna
+                      alignItems: 'stretch',
+                      
+                      backgroundColor: 'white'}}>
         
-        <View style= {{height: 65, backgroundColor: '#222'}}></View>
-        <View style= {{flex: 1, backgroundColor: 'white'}}></View>
-        <View style= {{height: 65, backgroundColor: '#222'}}>
-        </View>
+        <View style= {{height: 50, backgroundColor: 'green'}}></View>
+        <View style= {{height: 50, backgroundColor: 'red'}}></View>
+        <View style= {{height: 50, backgroundColor: 'yellow'}}></View>
+        <View style= {{height: 50, backgroundColor: 'green'}}></View>
+
+
 
       </View>
     );
