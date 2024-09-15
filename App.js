dd64a1355f2702f416c5ee1e9bfb5709a1cb220d
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2024-09-15 12:51:22 -0300

    Entendendo o modelo flex

--- App.js
+++ App.js
@@ -5,27 +5,21 @@ import { View, Text, StyleSheet } from "react-native";
 class App extends Component{
   render(){
     return(
-      <View style= {styles.area}>
-        <Text style={[ styles.textoPrincipal, styles.alinharTexto ]}>Eu sou texto 1</Text>
-        <Text style = {styles.alinharTexto}>Eu sou texto 2</Text>
-        <Text >Eu sou texto 3</Text>
-        <Text style = {styles.textoPrincipal}>Eu sou texto 4</Text>
+      <View style = {{flex: 1, backgroundColor: '#222'}}>
+        
+        <View style= {{flex: 1, backgroundColor: 'red'}}></View>
+        <View style= {{flex: 1, backgroundColor: 'green'}}>
+          <Text>Meu primeiro texto aqui</Text>
+        </View>
+        <View style= {{flex: 1, backgroundColor: 'yellow'}}>
+          <Text>Meu primeiro texto aqui</Text>
+        </View>
+        <View style= {{flex: 1, backgroundColor: 'blue'}}></View>
+
       </View>
     );
 }
 }
 
-const styles = StyleSheet.create({
-  area: {
-    marginTop: 40
-  },
-  textoPrincipal:{
-    fontSize: 25,
-    color: '#ff0000'
-  },
-  alinharTexto:{
-    textAlign: 'center'
-  }
-});
 
 export default App;
